a080f0166ad92337e4cfa53317787617015954e7
Anna <annaprando04@gmail.com>
2025-12-23 09:03:27 -0300

    feat(freelancer): registra module, controller e service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { FreelancerModule } from './freelancer/freelancer.module';
 
 @Module({
   imports: [PrismaModule, FreelancerModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- backend/src/freelancer/freelancer.module.ts
+++ backend/src/freelancer/freelancer.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FreelancerService } from './freelancer.service';
 import { FreelancerController } from './freelancer.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [FreelancerService],
   controllers: [FreelancerController]
 })
