f5077dac79cc9e370bee58fc23db7aaf7e437bcb
Anna <annaprando04@gmail.com>
2025-12-23 08:39:03 -0300

    feat(freelancer): adiciona DTOs com validação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { FreelancerModule } from './freelancer/freelancer.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, FreelancerModule],
   controllers: [],
   providers: [],
 })
