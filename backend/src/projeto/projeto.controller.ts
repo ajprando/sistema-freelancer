47599d4b485c861e910b1bd494a4ee943d83300f
Anna <annaprando04@gmail.com>
2025-12-24 10:08:03 -0300

    feat(projeto): implementa service e controller com prisma

---
+++ backend/src/projeto/projeto.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
+import { ProjetoService } from './projeto.service';
+import { CreateProjetoDto } from './dto/create-projeto.dto';
+import { UpdateProjetoDto } from './dto/update-projeto.dto';
+
+@Controller('projetos')
+export class ProjetoController {
+  constructor(private readonly projetoService: ProjetoService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createProjetoDto: CreateProjetoDto) {
+    return this.projetoService.create(createProjetoDto);
+  }
+
+  @Get()
+  findAll(
+    @Query('freelancerId') freelancerId?: string,
+    @Query('clienteId') clienteId?: string,
+  ) {
+    if (freelancerId) {
+      return this.projetoService.findByFreelancer(freelancerId);
+    }
+
+    if (clienteId) {
+      return this.projetoService.findByCliente(clienteId);
+    }
+
+    return this.projetoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.projetoService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateProjetoDto: UpdateProjetoDto,
+  ) {
+    return this.projetoService.update(id, updateProjetoDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.projetoService.remove(id);
+  }
+}
--- backend/src/projeto/projeto.module.ts
+++ backend/src/projeto/projeto.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProjetoService } from './projeto.service';
+import { ProjetoController } from './projeto.controller';
 
-@Module({})
+@Module({
+  providers: [ProjetoService],
+  controllers: [ProjetoController]
+})
 export class ProjetoModule {}
