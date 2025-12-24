04297ceb6ac3ac66a87935b00c0b7185ab88d83d
Anna <annaprando04@gmail.com>
2025-12-24 10:12:03 -0300

    feat(projeto): registra module, controller e service

--- backend/src/projeto/projeto.module.ts
+++ backend/src/projeto/projeto.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjetoService } from './projeto.service';
 import { ProjetoController } from './projeto.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ProjetoService],
   controllers: [ProjetoController]
 })
