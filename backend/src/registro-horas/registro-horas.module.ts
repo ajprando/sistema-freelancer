3aad1b2b47abf3fb8060d7199cc86d91c2203f49
Anna <annaprando04@gmail.com>
2025-12-27 12:10:36 -0300

    feat(registro-horas): registra module, controller e service

--- backend/src/registro-horas/registro-horas.module.ts
+++ backend/src/registro-horas/registro-horas.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RegistroHorasService } from './registro-horas.service';
 import { RegistroHorasController } from './registro-horas.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [RegistroHorasService],
   controllers: [RegistroHorasController]
 })
