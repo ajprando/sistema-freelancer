618b160ee3d1153189919e680766a749ea98b4e7
Anna <annaprando04@gmail.com>
2025-12-27 23:01:27 -0300

    feat(pagamento): registra module, controller e service

--- backend/src/pagamento/pagamento.module.ts
+++ backend/src/pagamento/pagamento.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PagamentoService } from './pagamento.service';
 import { PagamentoController } from './pagamento.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [PagamentoService],
   controllers: [PagamentoController]
 })
