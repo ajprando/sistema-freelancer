bc8edbe81a8615a2d7e2cf05c962bed562b0fab7
Anna <annaprando04@gmail.com>
2026-01-03 22:17:16 -0300

    feat(auth): implementa JwtStrategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET as string,
+      signOptions: { expiresIn: parseInt(process.env.JWT_EXPIRATION || '86400', 10) },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, PrismaService],
+  controllers: [AuthController],
+  exports: [AuthService, JwtModule, PassportModule],
 })
 export class AuthModule {}
